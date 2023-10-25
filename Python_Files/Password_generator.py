4876465933865d9cb50d0fc000b4a4c3e9504d0b
bdaly3412 <bdaly3411@gmail.com>
2023-10-25 14:36:17 +0100

    I added the Password_generator Python_Files Incase anyone needs to use it

---
+++ Python_Files/Password_generator.py
@@ -0,0 +1,26 @@
+import secrets
+import string
+
+
+pass_length = input("What length password would you like? ")
+
+random_token = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(int(pass_length)))
+print(random_token)
+
+
+output = random_token
+
+file_path = "passes.txt"
+
+with open(file_path, 'w') as file:
+    file.write("-" + output)
+
+print("Token written to file")
+
+
+
+my_dictionary = {
+    "name": "Aryas",
+    "age": 16
+}
+print(my_dictionary["name"])
